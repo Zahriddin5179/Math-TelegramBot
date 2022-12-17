ce5bbc2f2ee2fe0e647e4553ea9ae78fd05e3d33
Zahriddin5179 <zakhiriddinkhasanov013@gmail.com>
2022-12-17 05:34:30 +0500

    First commit but no changes

---
+++ src/test/java/uz/zahriddin/mathtelegrambot/MathTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.zahriddin.mathtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
